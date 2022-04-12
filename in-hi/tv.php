7c2a8bd023dffee2d8ecfbb4fd4477c1b3d20f3b
unknown <dipeshsinghpawar@gmail.com>
2022-04-12 08:52:11 +0530

    commit number 4

--- in-hi/tv.php
+++ in-hi/tv.php
@@ -2,7 +2,7 @@
     <div class="tv__flex">
         <div class="tv__text">
             <h1 class="tv__title">अपनी टीवी पर मज़ा लें.</h1>
-            <h2 class="tv__desc">स्मार्ट टीवी, PlayStation, Xbox, Chromecast, Apple TV, Blu-ray प्लेयर के साथ ही दूसरे डिवाइस पर भी देखें.</h2>
+            <h2 class="tv__desc">स्मार्ट टीवी, PlayStation, Xbox, Chromecast, Blu-ray प्लेयर के साथ ही दूसरे डिवाइस पर भी देखें.</h2>
         </div>
         <div class="tv__anim_container">
             <img alt="tv image" class="tv__img" src="/int220/imgs/tv.png">
