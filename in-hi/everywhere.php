8ffe3271f6d880b69c32f18dd2f4d850c861701a
unknown <dipeshsinghpawar@gmail.com>
2022-04-12 08:52:33 +0530

    commit number 5

--- in-hi/everywhere.php
+++ in-hi/everywhere.php
@@ -2,7 +2,7 @@
     <div class="tv__flex mobile_flex">
         <div class="tv__text">
             <h1 class="tv__title">हर जगह देखें.</h1>
-            <h2 class="tv__desc">बिना ज़्यादा पेमेंट किए, अपने फ़ोन, टैबलेट, लैपटॉप और टीवी पर अनलिमिटेड फ़िल्में और टीवी शो स्ट्रीम करें.</h2>
+            <h2 class="tv__desc">बिना ज़्यादा पेमेंट किए, अपने फ़ोन,टैबलेट, लैपटॉप और टीवी पर अनलिमिटेड फ़िल्में और टीवी शो स्ट्रीम करें.</h2>
         </div>
         <div class="tv__anim_container">
             <img alt="tv image" class="tv__img" src="/int220/imgs/device-pile-in.png">
